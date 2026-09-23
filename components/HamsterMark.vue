d351e33e98e532235e46f5e3c3a801fd4429d9f1
Ryoga Takahira <ryoga.takahira@gmail.com>
2026-09-23 21:39:34 +0900

    fix: resolve image paths with BASE_URL for GitHub Pages

--- components/HamsterMark.vue
+++ components/HamsterMark.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+const src = computed(() => {
+  const base = import.meta.env.BASE_URL
+  const path = '/images/hamster-geometric.png'
+  return base && base !== '/' ? base.replace(/\/$/, '') + path : path
+})
+</script>
+
 <template>
-  <img class="hamster-mark" src="/images/hamster-geometric.png" alt="" aria-hidden="true" />
+  <img class="hamster-mark" :src="src" alt="" aria-hidden="true" />
 </template>
